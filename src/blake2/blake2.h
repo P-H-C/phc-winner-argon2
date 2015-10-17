f288d9c4484f80d1b36c92e248fc83d2310e802e
JP Aumasson <jeanphilippe.aumasson@gmail.com>
2015-10-17 16:58:37 +0200

    removed b2s b2sp b2bp stuff

--- src/blake2/blake2.h
+++ src/blake2/blake2.h
@@ -29,15 +29,6 @@
 extern "C" {
 #endif
 
-enum blake2s_constant
-{
-    BLAKE2S_BLOCKBYTES = 64,
-    BLAKE2S_OUTBYTES   = 32,
-    BLAKE2S_KEYBYTES   = 32,
-    BLAKE2S_SALTBYTES  = 8,
-    BLAKE2S_PERSONALBYTES = 8
-};
-
 enum blake2b_constant
 {
     BLAKE2B_BLOCKBYTES = 128,
@@ -48,31 +39,6 @@ enum blake2b_constant
 };
 
 #pragma pack(push, 1)
-typedef struct __blake2s_param
-{
-    uint8_t  digest_length; // 1
-    uint8_t  key_length;    // 2
-    uint8_t  fanout;        // 3
-    uint8_t  depth;         // 4
-    uint32_t leaf_length;   // 8
-    uint8_t  node_offset[6];// 14
-    uint8_t  node_depth;    // 15
-    uint8_t  inner_length;  // 16
-    // uint8_t  reserved[0];
-    uint8_t  salt[BLAKE2S_SALTBYTES]; // 24
-    uint8_t  personal[BLAKE2S_PERSONALBYTES];  // 32
-} blake2s_param;
-
-ALIGN( 64 ) typedef struct __blake2s_state
-{
-    uint32_t h[8];
-    uint32_t t[2];
-    uint32_t f[2];
-    uint8_t  buf[2 * BLAKE2S_BLOCKBYTES];
-    size_t   buflen;
-    uint8_t  last_node;
-} blake2s_state;
-
 typedef struct __blake2b_param
 {
     uint8_t  digest_length; // 1
@@ -98,29 +64,7 @@ ALIGN( 64 ) typedef struct __blake2b_state
     uint8_t  last_node;
 } blake2b_state;
 
-ALIGN( 64 ) typedef struct __blake2sp_state
-{
-    blake2s_state S[8][1];
-    blake2s_state R[1];
-    uint8_t buf[8 * BLAKE2S_BLOCKBYTES];
-    size_t  buflen;
-} blake2sp_state;
-
-ALIGN( 64 ) typedef struct __blake2bp_state
-{
-    blake2b_state S[4][1];
-    blake2b_state R[1];
-    uint8_t buf[4 * BLAKE2B_BLOCKBYTES];
-    size_t  buflen;
-} blake2bp_state;
 #pragma pack(pop)
-
-// Streaming API
-int blake2s_init( blake2s_state *S, const uint8_t outlen );
-int blake2s_init_key( blake2s_state *S, const uint8_t outlen, const void *key, const uint8_t keylen );
-int blake2s_init_param( blake2s_state *S, const blake2s_param *P );
-int blake2s_update( blake2s_state *S, const uint8_t *in, uint64_t inlen );
-int blake2s_final( blake2s_state *S, uint8_t *out, uint8_t outlen );
 
 int blake2b_init( blake2b_state *S, const uint8_t outlen );
 int blake2b_init_key( blake2b_state *S, const uint8_t outlen, const void *key, const uint8_t keylen );
@@ -128,30 +72,12 @@ int blake2b_init_param( blake2b_state *S, const blake2b_param *P );
 int blake2b_update( blake2b_state *S, const uint8_t *in, uint64_t inlen );
 int blake2b_final( blake2b_state *S, uint8_t *out, uint8_t outlen );
 
-int blake2sp_init( blake2sp_state *S, const uint8_t outlen );
-int blake2sp_init_key( blake2sp_state *S, const uint8_t outlen, const void *key, const uint8_t keylen );
-int blake2sp_update( blake2sp_state *S, const uint8_t *in, uint64_t inlen );
-int blake2sp_final( blake2sp_state *S, uint8_t *out, uint8_t outlen );
 
-int blake2bp_init( blake2bp_state *S, const uint8_t outlen );
-int blake2bp_init_key( blake2bp_state *S, const uint8_t outlen, const void *key, const uint8_t keylen );
-int blake2bp_update( blake2bp_state *S, const uint8_t *in, uint64_t inlen );
-int blake2bp_final( blake2bp_state *S, uint8_t *out, uint8_t outlen );
-
-// Simple API
-int blake2s( uint8_t *out, const void *in, const void *key, const uint8_t outlen, const uint64_t inlen, uint8_t keylen );
 int blake2b( uint8_t *out, const void *in, const void *key, const uint8_t outlen, const uint64_t inlen, uint8_t keylen );
 /* Argon2 Team - Begin Code */
 int blake2b_long( uint8_t *out, const void *in, const uint32_t outlen, const uint64_t inlen );
 /* Argon2 Team - End Code */
 
-int blake2sp( uint8_t *out, const void *in, const void *key, const uint8_t outlen, const uint64_t inlen, uint8_t keylen );
-int blake2bp( uint8_t *out, const void *in, const void *key, const uint8_t outlen, const uint64_t inlen, uint8_t keylen );
-
-static inline int blake2( uint8_t *out, const void *in, const void *key, const uint8_t outlen, const uint64_t inlen, uint8_t keylen )
-{
-    return blake2b( out, in, key, outlen, inlen, keylen );
-}
 
 #if defined(__cplusplus)
 }
