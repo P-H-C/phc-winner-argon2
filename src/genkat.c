a5993bc689a7003a17fefd62ca125e0ca075db0d
JP Aumasson <jeanphilippe.aumasson@gmail.com>
2015-11-04 13:51:16 +0100

    missing (c) header

--- src/genkat.c
+++ src/genkat.c
@@ -1,3 +1,16 @@
+/*
+ * Argon2 source code package
+ *
+ * Written by Daniel Dinu and Dmitry Khovratovich, 2015
+ *
+ * This work is licensed under a Creative Commons CC0 1.0 License/Waiver.
+ *
+ * You should have received a copy of the CC0 Public Domain Dedication along
+ * with
+ * this software. If not, see
+ * <http://creativecommons.org/publicdomain/zero/1.0/>.
+ */
+
 #include <inttypes.h>
 #include <stdio.h>
 #include <stdlib.h>
