9099d2da6e80881204b72705556937eba4a4a816
Ilya Moroz <flamewow@yandex.ru>
2015-12-02 08:52:21 +0200

    some refactor around argon_type and raising exception

--- bindings/python/argon2.py
+++ bindings/python/argon2.py
@@ -29,18 +29,23 @@ _crypto_argon2.argtypes = [
 _crypto_argon2.restype = c_int
 
 
+class Argon2Type(object):
+    Argon2_d = 0
+    Argon2_i = 1
+
+
 class Argon2Exception(Exception):
-    error = ("ARGON2_OK", "ARGON2_OUTPUT_PTR_NULL", "ARGON2_OUTPUT_TOO_SHORT", "ARGON2_OUTPUT_TOO_LONG",
-             "ARGON2_PWD_TOO_SHORT", "ARGON2_PWD_TOO_LONG", "ARGON2_SALT_TOO_SHORT",
-             "ARGON2_SALT_TOO_LONG", "ARGON2_AD_TOO_SHORT", "ARGON2_AD_TOO_LONG",
-             "ARGON2_SECRET_TOO_SHORT", "ARGON2_SECRET_TOO_LONG",
-             "ARGON2_TIME_TOO_SMALL", "ARGON2_TIME_TOO_LARGE", "ARGON2_MEMORY_TOO_LITTLE",
-             "ARGON2_MEMORY_TOO_MUCH", "ARGON2_LANES_TOO_FEW", "ARGON2_LANES_TOO_MANY",
-             "ARGON2_PWD_PTR_MISMATCH", "ARGON2_SALT_PTR_MISMATCH", "ARGON2_SECRET_PTR_MISMATCH",
-             "ARGON2_AD_PTR_MISMATCH", "ARGON2_MEMORY_ALLOCATION_ERROR", "ARGON2_FREE_MEMORY_CBK_NULL",
-             "ARGON2_ALLOCATE_MEMORY_CBK_NULL", "ARGON2_INCORRECT_PARAMETER", "ARGON2_INCORRECT_TYPE",
-             "ARGON2_OUT_PTR_MISMATCH", "ARGON2_THREADS_TOO_FEW", "ARGON2_THREADS_TOO_MANY",
-             "ARGON2_MISSING_ARGS", "ARGON2_ENCODING_FAIL", "ARGON2_DECODING_FAIL")
+    errors = ("ARGON2_OK", "ARGON2_OUTPUT_PTR_NULL", "ARGON2_OUTPUT_TOO_SHORT", "ARGON2_OUTPUT_TOO_LONG",
+              "ARGON2_PWD_TOO_SHORT", "ARGON2_PWD_TOO_LONG", "ARGON2_SALT_TOO_SHORT",
+              "ARGON2_SALT_TOO_LONG", "ARGON2_AD_TOO_SHORT", "ARGON2_AD_TOO_LONG",
+              "ARGON2_SECRET_TOO_SHORT", "ARGON2_SECRET_TOO_LONG",
+              "ARGON2_TIME_TOO_SMALL", "ARGON2_TIME_TOO_LARGE", "ARGON2_MEMORY_TOO_LITTLE",
+              "ARGON2_MEMORY_TOO_MUCH", "ARGON2_LANES_TOO_FEW", "ARGON2_LANES_TOO_MANY",
+              "ARGON2_PWD_PTR_MISMATCH", "ARGON2_SALT_PTR_MISMATCH", "ARGON2_SECRET_PTR_MISMATCH",
+              "ARGON2_AD_PTR_MISMATCH", "ARGON2_MEMORY_ALLOCATION_ERROR", "ARGON2_FREE_MEMORY_CBK_NULL",
+              "ARGON2_ALLOCATE_MEMORY_CBK_NULL", "ARGON2_INCORRECT_PARAMETER", "ARGON2_INCORRECT_TYPE",
+              "ARGON2_OUT_PTR_MISMATCH", "ARGON2_THREADS_TOO_FEW", "ARGON2_THREADS_TOO_MANY",
+              "ARGON2_MISSING_ARGS", "ARGON2_ENCODING_FAIL", "ARGON2_DECODING_FAIL")
 
 
 def _ensure_bytes(data):
@@ -53,22 +58,19 @@ def _ensure_bytes(data):
     return data
 
 
-def hash(password, salt, t=16 , r=8, p=1, buflen=128):
+def hash(password, salt, t=16 , m=8, p=1, buflen=128, argon_type=Argon2Type.Argon2_i):
     outbuf = create_string_buffer(buflen)
-    # argon2_d = 0
-    argon2_i = 1
     password = _ensure_bytes(password)
     salt = _ensure_bytes(salt)
 
-    result = _crypto_argon2(t, r, p,
+    result = _crypto_argon2(t, m, p,
                             password, len(password),
                             salt, len(salt),
                             outbuf, buflen,
                             None, 0,
-                            argon2_i)
+                            argon_type)
 
     if result:
-        print (Argon2Exception.error[result])
-        raise Argon2Exception('could not compute hash')
+        raise Argon2Exception(Argon2Exception.errors[result])
 
     return outbuf.raw
