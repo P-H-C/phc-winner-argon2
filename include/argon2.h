e1b67013d7e443872bbca0adf0d69142721e0c9f
Radosław Chmielarz <radoslaw.chmielarz-ext@leica-geosystems.com>
2023-05-29 14:10:35 +0200

    Further linux build fix

--- include/argon2.h
+++ include/argon2.h
@@ -18,7 +18,7 @@
 #ifndef ARGON2_H
 #define ARGON2_H
 
-#if (_MSC_VER < 1600)
+#if defined(_MSC_VER) && (_MSC_VER < 1600)
 typedef unsigned __int64 uint64_t;
 typedef unsigned __int32 uint32_t;
 typedef unsigned __int8 uint8_t;
